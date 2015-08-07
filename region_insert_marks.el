26fe3a8f2e9f341dd9b21ba53dc577d5c6e7e0ae
sjw <sunjiwen@gametree.com>
2015-08-07 18:14:23 +0800

    add method to insert ' and "

--- region_insert_marks.el
+++ region_insert_marks.el
@@ -14,4 +14,20 @@
     (goto-char start) (insert ")")
     ))
 
+(defun region-insert-quotes (start end)
+  "Insert a markup {} around a region."
+  (interactive "r")
+  (save-excursion
+    (goto-char end) (insert "\"")
+    (goto-char start) (insert "\"")
+    ))
+
+(defun region-insert-single-quotes (start end)
+  "Insert a markup {} around a region."
+  (interactive "r")
+  (save-excursion
+    (goto-char end) (insert "'")
+    (goto-char start) (insert "'")
+    ))
+
 (provide 'region_insert_marks)
